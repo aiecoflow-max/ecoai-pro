3ee97d8d3c51cc2008fa2e20f598819db7e04c5a
EcoFlow <aiecoflow@gmail.com>
2026-03-23 19:45:42 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route, useLocation, useNavigate } from 'react-router-dom'
-import Home from './pages/Home'
-import Tool from './pages/Tool'
-import Layout from './components/Layout'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-function App() {
-  const location = useLocation()
-  const navigate = useNavigate()
-  const isKorean = location.pathname.startsWith('/ko')
-
-  const toggleLang = () => {
-    const newPath = isKorean
-      ? location.pathname.replace('/ko', '') || '/'
-      : '/ko' + location.pathname
-    navigate(newPath)
-  }
-
-  return (
-    <Layout isKorean={isKorean} onToggleLang={toggleLang}>
-      <Routes>
-        <Route path="/" element={<Home lang="en" />} />
-        <Route path="/ko" element={<Home lang="ko" />} />
-        <Route path="/tool" element={<Tool />} />
-        <Route path="/ko/tool" element={<Tool />} />
-      </Routes>
-    </Layout>
-  )
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>
-)
+);
